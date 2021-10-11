2692e486a6a04ef25e2f3fbdd51b793f7f7488e9
Ana Araújo <anapro1141@gmail.com>
2021-10-11 18:49:53 -0300

    cria execicio 1001

---
+++ exercicios_c/exercicio_1001.c
@@ -0,0 +1,18 @@
+/* Problema 1001 - URI Online Judge */
+/* Programa realiza a soma de dois números inteiros */
+
+#include <stdio.h>
+
+int main() {
+ 
+    int a, b, x;
+   
+    //printf("Insira dois números: ");
+    scanf("%d %d", &a, &b);
+   
+    x = a + b;
+   
+    printf("X = %d\n", x);
+   
+    return 0;
+}
