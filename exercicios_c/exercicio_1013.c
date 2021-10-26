79e42b5bbc3884d7c70512d298600c0dd7c454d7
Ana Araújo <anapro1141@gmail.com>
2021-10-26 20:41:31 -0300

    cria exercicio 1013 - maior numero

---
+++ exercicios_c/exercicio_1013.c
@@ -0,0 +1,25 @@
+/* Problema 1013 URI Online Judge */
+/* Problema maior entre 3 nºs */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    
+    int a, b, c, maior_ab, maior;
+    
+    //printf("Insira 3 nºs: ");
+    scanf("%d %d %d", &a, &b, &c);
+    
+    maior_ab = (a+b+abs(a-b)) / 2;
+    
+    if(c > maior_ab){
+        maior = c;
+    } else {
+        maior = maior_ab;
+    }
+    
+    printf("%d eh o maior\n", maior);
+    
+    return 0;
+}
