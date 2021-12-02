285276f70685e5d1679f1eda1d16fe38b8cabe10
ana araujo <analiviaraujopro@gmail.com>
2021-12-02 14:10:21 -0300

    adiciona ex 1073 - quadrado de pares

---
+++ exercicios_c/exercicio_1073.c
@@ -0,0 +1,19 @@
+// Problema 1073  - beecrowd 
+//Programa calcula quadrado de números pares
+
+#include<stdio.h>
+
+int main(){
+
+    int num, i, resultado;
+
+    scanf("%d", &num);
+
+    for(i = 2; i <= num; i++){
+        if(num % 2 == 0){
+            printf("%d^2 = %d\n", i++, i*i);
+        }
+    }
+    
+    return 0;
+}  
