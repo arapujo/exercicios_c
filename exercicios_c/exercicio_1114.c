96a7ef83f37f80a392f08d8205354e0bbc3e63e5
ana araujo <analiviaraujopro@gmail.com>
2021-12-02 14:23:23 -0300

    adiciona ex 1114 - senha fixa

---
+++ exercicios_c/exercicio_1114.c
@@ -0,0 +1,22 @@
+// Problema 1114  - beecrowd 
+//Programa verifica senha
+
+#include<stdio.h>
+
+int main(){
+
+    int senha, senha_correta;
+
+    do{
+        scanf("%d", &senha);
+
+        if(senha != 2002){
+            printf("Senha invalida\n");
+        } else {
+            printf("Acesso permitido\n");
+        }
+    }
+    while(senha!=2002);
+
+    return 0;
+}  
