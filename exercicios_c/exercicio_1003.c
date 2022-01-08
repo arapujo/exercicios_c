2cd8e610692a4f33510a2dd54d036e491ce6491f
ana araujo <analiviaraujopro@gmail.com>
2022-01-08 15:40:44 -0300

    adicioa ex 1003 - area circulo

---
+++ exercicios_c/exercicio_1003.c
@@ -0,0 +1,17 @@
+//Problema 1003 - beecrownd
+//Programa realiza soma de dois números inteiros
+
+#include<stdio.h>
+
+int main(){
+
+    int n1, n2, soma;
+
+    scanf("%d %d", &n1, &n2);
+
+    soma = n1 + n2;
+
+    printf("SOMA = %d\n", soma);
+
+    return 0;
+}
